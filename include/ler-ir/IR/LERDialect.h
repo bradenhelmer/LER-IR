5abeef02d991cbb7a4d5d9c34f268f49179036f5
Braden Helmer <bradenhelmeraus@gmail.com>
2024-08-27 15:39:11 -0400

    Remove unecessary includes

--- include/ler-ir/IR/LERDialect.h
+++ include/ler-ir/IR/LERDialect.h
@@ -7,16 +7,6 @@
 #include <mlir/IR/Dialect.h>
 
 #include <ler-ir/IR/LERDialect.h.inc>
-#include <mlir/Dialect/Affine/IR/AffineMemoryOpInterfaces.h>
-#include <mlir/Dialect/Arith/Utils/Utils.h>
-#include <mlir/Dialect/Utils/StaticValueUtils.h>
-#include <mlir/Interfaces/ControlFlowInterfaces.h>
-#include <mlir/Interfaces/DestinationStyleOpInterface.h>
-#include <mlir/Interfaces/InferTypeOpInterface.h>
-#include <mlir/Interfaces/LoopLikeInterface.h>
-#include <mlir/Interfaces/ParallelCombiningOpInterface.h>
-#include <mlir/IR/PatternMatch.h>
-#include <mlir/IR/Region.h>
 
 #define GET_OP_CLASSES
 #include <ler-ir/IR/LEROps.h.inc>
