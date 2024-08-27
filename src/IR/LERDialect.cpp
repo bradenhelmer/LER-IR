dd8e15254ba3d237edc5639b68cb7a0bdb50f9c8
Braden Helmer <bradenhelmeraus@gmail.com>
2024-08-27 15:37:00 -0400

    Dialect actually compiling

--- src/IR/LERDialect.cpp
+++ src/IR/LERDialect.cpp
@@ -1,1 +1,15 @@
+// LERDialect.cpp
+// ~~~~~~~~~~~~~~
+// Core LER dialect implementations.
 #include <ler-ir/IR/LERDialect.h>
+#include <mlir/Dialect/Affine/IR/AffineOps.h>
+#include <mlir/Dialect/SCF/IR/SCF.h>
+
+#include <ler-ir/IR/LERDialect.cpp.inc>
+using namespace mlir::ler;
+void LERDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include <ler-ir/IR/LEROps.cpp.inc>
+      >();
+}
